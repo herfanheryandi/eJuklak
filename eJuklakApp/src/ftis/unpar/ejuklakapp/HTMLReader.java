d92a72cd439afeb260e00513eb9556241e1a4b83
herfan <herfanheryandi@gmail.com>
2015-02-23 18:51:17 +0700

    Menambahkan method read()

--- eJuklakApp/src/ftis/unpar/ejuklakapp/HTMLReader.java
+++ eJuklakApp/src/ftis/unpar/ejuklakapp/HTMLReader.java
@@ -8,4 +8,15 @@ public class HTMLReader {
 	public HTMLReader(String file) throws FileNotFoundException{
 		reader = new BufferedReader(new FileReader(new File(file)));
 	}
+	
+	public String read() throws IOException{
+		String res = new String();
+		/*String text = reader.readLine();
+		while(text!=null){
+			res += text;
+			text = reader.readLine();
+		}*/
+		reader.close();
+		return res;
+	}
 }
