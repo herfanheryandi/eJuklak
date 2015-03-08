d482545c7f494cf12bbf5e56c6765cf696da3f83
herfanheryandi <herfanheryandi@gmail.com>
2015-03-08 21:22:21 +0700

    Refresh

--- eJuklakApp/src/ftis/unpar/ejuklakapp/MainActivity.java
+++ eJuklakApp/src/ftis/unpar/ejuklakapp/MainActivity.java
@@ -28,12 +28,7 @@ import android.text.Html.ImageGetter;
 
 
 public class MainActivity extends ActionBarActivity {
-	 private String[] mPlanetTitles;
-	 private DrawerLayout mDrawerLayout;
-	 private ListView mDrawerList;
-	 private ActionBarDrawerToggle mDrawerToggle;
-	 private CharSequence mDrawerTitle;
-	 private CharSequence mTitle;
+
 	
 	
     @Override
@@ -45,44 +40,7 @@ public class MainActivity extends ActionBarActivity {
         WebView myWebView = (WebView) findViewById(R.id.webview);
         myWebView.loadUrl("file:///android_asset/MarkdownBab1.htm");
         /*navigation drawer*/
-        mPlanetTitles= getResources().getStringArray(R.array.navigation_drawer_items_array);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        ObjectDrawerItem[] drawerItem = new ObjectDrawerItem[3];
-        
-        drawerItem[0] = new ObjectDrawerItem(R.drawable.ic_launcher, "Create");
-        drawerItem[1] = new ObjectDrawerItem(R.drawable.ic_launcher, "Read");
-        drawerItem[2] = new ObjectDrawerItem(R.drawable.ic_launcher, "Help");
-        DrawerItemCustomAdapter adapter = new DrawerItemCustomAdapter(this, R.layout.listview_item_row, drawerItem);
-        mDrawerList.setAdapter(adapter);
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-        mTitle = mDrawerTitle = getTitle();
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerToggle = new ActionBarDrawerToggle(
-                this,
-                mDrawerLayout,
-                R.drawable.ic_launcher, 
-                R.string.drawer_open, 
-                R.string.drawer_close 
-                ) {
-            
-            /** Called when a drawer has settled in a completely closed state. */
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                getActionBar().setTitle(mTitle);
-            }
-         
-            /** Called when a drawer has settled in a completely open state. */
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                getActionBar().setTitle(mDrawerTitle);
-            }
-        };
-         
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-         
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        getActionBar().setHomeButtonEnabled(true);
+
     }
 
     @Override
@@ -102,65 +60,8 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
         return super.onOptionsItemSelected(item);
     }
     
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectItem(position);
-        }
-        
-    }
-     
-    private void selectItem(int position) {
-        
-        Fragment fragment = null;
-        
-        switch (position) {
-        case 0:
-            fragment = new CustomFragment();
-            break;
-        case 1:
-            fragment = new CustomFragment();
-            break;
-        case 2:
-            fragment = new CustomFragment();
-            break;
-     
-        default:
-            break;
-        }
-        
-        if (fragment != null) {
-            android.app.FragmentManager fragmentManager = getFragmentManager();
-            fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
-     
-            mDrawerList.setItemChecked(position, true);
-            mDrawerList.setSelection(position);
-            //getActionBar().setTitle(mPlanetTitles[position]);
-            setTitle(mPlanetTitles[position]);
-            mDrawerLayout.closeDrawer(mDrawerList);
-            
-        } else {
-            Log.e("MainActivity", "Error in creating fragment");
-        }
-    }
-    
-    @Override
-    public void setTitle(CharSequence title) {
-        mTitle = title;
-        getActionBar().setTitle(mTitle);
-    }
-    
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        mDrawerToggle.syncState();
-    }
     
 }
