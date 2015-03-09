9cbca4178ab3d6e3202ac15103aa2c4a65e3fee7
herfan <herfanheryandi@gmail.com>
2015-03-09 09:12:59 +0700

    Nav Drawer Normal

---
+++ eJuklakApp/src/ftis/unpar/ejuklakapp/DrawerItemCustomAdapter.java
@@ -0,0 +1,45 @@
+package ftis.unpar.ejuklakapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DrawerItemCustomAdapter extends ArrayAdapter<ObjectDrawerItem> {
+	 
+    Context mContext;
+    int layoutResourceId;
+    ObjectDrawerItem data[] = null;
+ 
+    public DrawerItemCustomAdapter(Context mContext, int layoutResourceId, ObjectDrawerItem[] data) {
+ 
+        super(mContext, layoutResourceId, data);
+        this.layoutResourceId = layoutResourceId;
+        this.mContext = mContext;
+        this.data = data;
+    }
+ 
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+ 
+        View listItem = convertView;
+ 
+        LayoutInflater inflater =LayoutInflater.from(mContext);
+        listItem = inflater.inflate(layoutResourceId, parent, false);
+ 
+        ImageView imageViewIcon = (ImageView) listItem.findViewById(R.id.imageViewIcon);
+        TextView textViewName = (TextView) listItem.findViewById(R.id.textViewName);
+        
+        ObjectDrawerItem folder = data[position];
+ 
+        
+        imageViewIcon.setImageResource(folder.icon);
+        textViewName.setText(folder.name);
+        
+        return listItem;
+    }
+ 
+}
--- eJuklakApp/src/ftis/unpar/ejuklakapp/MainActivity.java
+++ eJuklakApp/src/ftis/unpar/ejuklakapp/MainActivity.java
@@ -1,20 +1,35 @@
 package ftis.unpar.ejuklakapp;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 
 
 public class MainActivity extends ActionBarActivity {
 	private WebView webView;
+	private String[] mNavigationDrawerItemTitles;
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	ActionBarDrawerToggle mDrawerToggle;
+	private CharSequence mDrawerTitle;
+	private CharSequence mTitle;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTitle = mDrawerTitle = getTitle();
         //Code here
         
         /*webview*/
@@ -22,7 +37,89 @@ public class MainActivity extends ActionBarActivity {
         webView.loadUrl("file:///android_asset/MarkdownBab1.htm");
         
         /*navigation drawer*/ 
+        mNavigationDrawerItemTitles= getResources().getStringArray(R.array.navigation_drawer_items_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
         
+        ObjectDrawerItem[] drawerItem = new ObjectDrawerItem[3];
+        
+        drawerItem[0] = new ObjectDrawerItem(R.drawable.ic_paper, "Create");
+        drawerItem[1] = new ObjectDrawerItem(R.drawable.ic_paper, "Read");
+        drawerItem[2] = new ObjectDrawerItem(R.drawable.ic_paper, "Help");
+        
+        DrawerItemCustomAdapter adapter = new DrawerItemCustomAdapter(this, R.layout.listview_item_row, drawerItem);
+        mDrawerList.setAdapter(adapter);
+        
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                R.drawable.ic_menu, 
+                R.string.drawer_open, 
+                R.string.drawer_close 
+                ) {
+            
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getActionBar().setTitle(mTitle);
+            }
+         
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActionBar().setTitle(mDrawerTitle);
+            }
+        };
+         
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+         
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+    }
+    
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+        
+    }
+     
+    private void selectItem(int position) {
+        
+        Fragment fragment = null;
+        
+        switch (position) {
+        case 0:
+            fragment = new CreateFragment();
+            break;
+        case 1:
+            fragment = new CreateFragment();
+            break;
+        case 2:
+            fragment = new CreateFragment();
+            break;
+     
+        default:
+            break;
+        }
+        
+        if (fragment != null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+     
+            mDrawerList.setItemChecked(position, true);
+            mDrawerList.setSelection(position);
+            setTitle(mNavigationDrawerItemTitles[position]);
+            mDrawerLayout.closeDrawer(mDrawerList);
+            
+        } else {
+            Log.e("MainActivity", "Error in creating fragment");
+        }
     }
 
     @Override
@@ -41,9 +138,23 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
-     
+    
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getActionBar().setTitle(mTitle);
+    }
+    
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
 }
 
 
